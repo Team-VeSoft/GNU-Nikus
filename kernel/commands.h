380f9e83d5aa4b1515f44c1a4eed7cc579f008d4
Vladimir Vlaicu <93855040+Vovo4k@users.noreply.github.com>
2023-08-07 13:20:44 +0300

    Create commands.h

---
+++ kernel/commands.h
@@ -0,0 +1,9 @@
+// commands.h - Заголовочный файл для команд
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+// Функция для обработки команд
+void handle_command(const char* command);
+
+#endif // COMMANDS_H
